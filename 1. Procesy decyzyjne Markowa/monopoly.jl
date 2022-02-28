cc884a6edba722090d002f0ece8254c4f75b1254
bartoszpankratz <32309411+bartoszpankratz@users.noreply.github.com>
2022-02-28 09:14:51 -0500

    Update monopoly.jl

--- 1. Procesy decyzyjne Markowa/monopoly.jl
+++ 1. Procesy decyzyjne Markowa/monopoly.jl
@@ -1,5 +1,5 @@
-
 using DataFrames
+using Random
 
             Spaces = [
                 ("Go", 0.0, Inf),
@@ -54,15 +54,15 @@ function transition_matrix(n=40)
     #special matrix
     S = zeros(Float64,n,n)
     for i = 1:n
-        if !in(i,(3,18,30,33))
+        if !in(i,(3,18,31,34))
             S[i,i] = 1.0
-        elseif i == 30
-            S[i,10] = 1.0
+        elseif i == 31
+            S[i,11] = 1.0
         else #community chest
             #advance to go
             S[i,1] = 1.0 / 16.0;
             #go to jail
-            S[i,10] = 1.0 / 16.0;
+            S[i,11] = 1.0 / 16.0;
             #stay put
             S[i,i] = 14.0 / 16.0;
         end
@@ -76,6 +76,10 @@ function solve(k,n=40)
     s * T^k
 end
 
+#first as a simple Markov chain:
+solve(100000,40)'
+
+#add some rewards:
 function summary(k = 100000, n=40)
     probs = solve(k,n)
     df = DataFrame(Space = String[], Prob = Float64[], Rent = Float64[], ROI = Float64[])
@@ -89,11 +93,66 @@ end
 
 summary()
 
-T = transition_matrix();
+#Monopoly simulaton with simple strategies
 
-s = hcat(1.0,zeros(Float64,1,39));
+function simulate_game(n = 40, Spaces = Spaces)
+    roi = summary()[!,:ROI]
+    owned = zeros(Int,n)
+    budget = 1500.0 * ones(3)
+    position = ones(Int,3)
+    seq = shuffle(1:3)
+    while sum(budget.> 0.0) > 1
+        for player in seq
+            budget[player] ≤ 0.0 && continue
+            roll = rand(2:12)
+            position[player] =  mod(position[player] + roll - 1,n) + 1
+            if position[player] == 31
+                position[player] = 11
+            elseif in(position[player],(3,18,34)) #community chest
+                if rand() ≤ 2.0/16.0 
+                    if rand() ≤ 0.5 #advance to go
+                        position[player] = 1
+                    else #go to jail
+                        position[player] = 11
+                    end
+                end
+            else
+                owner = owned[position[player]] 
+                (Spaces[position[player]][3] == Inf || owner == player) && continue
+                if owner == 0
+                    price = Spaces[position[player]][3]
+                    price ≥ budget[player] && continue
+                    #decision time
+                    if player == 1  &&  rand() ≤ 0.5 
+                        #random strategy (player 1)
+                        owned[position[player]] = player
+                        budget[player] -= price
+                    elseif player == 2 
+                        #always buy (player 2)
+                        owned[position[player]] = player
+                        budget[player] -= price
+                    elseif player == 3 && roi[position[player]] > 0.15 
+                        #buy only the best (player 3)
+                        owned[position[player]] = player
+                        budget[player] -= price
+                    end  
+                else
+                    rent = Spaces[position[player]][2]
+                    budget[player] -= rent
+                    budget[owner] += rent
+                end
+                budget[player] ≤ 0.0 && replace!(owned, player => 0)
+            end
+        end
+    end
+    return budget
+end
 
+simulate_game()
 
-
-
-
+n = 10000
+res = zeros(3)
+for k = 1:n
+   res .+= (simulate_game() .> 0.0)
+end
+res ./ n
